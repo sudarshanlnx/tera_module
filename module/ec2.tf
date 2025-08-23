4e0d9210e3ceb0965010f1b84a721cbf3e180701
sudarshan <sudarshanlnx@gmail.com>
2025-08-23 08:37:46 +0530

    new_terramodule

--- module/ec2.tf
+++ module/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "webserver" {
-  ami           = var.amiid
-  instance_type = var.machinetype
+  ami             = var.amiid
+  instance_type   = var.machinetype
   security_groups = [var.sgname]
-  key_name      = var.keyname
+  key_name        = var.keyname
 
   tags = {
     Name = var.mytag
